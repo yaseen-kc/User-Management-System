a811d1fcbfe5220f1cc667c0993c21a10ea20cc2
Muhammed Yaseen K C <ymkcmpd@gmail.com>
2024-02-13 10:31:49 +0530

    Server Creation and MongoDB connection
    // Initializes a MongoDB connection using Mongoose
    // Creates an Express server listening on port 3000.

---
+++ index.js
@@ -0,0 +1,13 @@
+// This program initializes a MongoDB connection using Mongoose
+// Creates an Express server listening on port 3000.
+
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://127.0.0.1:27017/UMS');
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, function () {
+    console.log(`App listening on port ${port}!`)
+});
