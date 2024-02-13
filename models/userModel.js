cd29190401fdfb262d30dc4ce8eee34e70903036
Muhammed Yaseen K C <ymkcmpd@gmail.com>
2024-02-13 10:49:13 +0530

    update userModel.js

---
+++ models/userModel.js
@@ -0,0 +1,38 @@
+// Define a Mongoose schema for user data, including fields 
+// for name, email, mobile, image, password, admin status, and verification status.
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    mobile: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    is_admin: {
+        type: Number,
+        required: true
+    },
+    is_verified: {
+        type: Number,
+        default: 0
+    }
+
+});
+// Export the Mongoose model for the User schema
+module.exports = mongoose.model('User', userSchema);
