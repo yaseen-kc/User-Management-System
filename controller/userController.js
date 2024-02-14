9439212cde668f4d29e04ae175e71d503b47f552
Muhammed Yaseen K C <ymkcmpd@gmail.com>
2024-02-14 10:36:04 +0530

    before Bug update userController.js and userRoute.js

--- controller/userController.js
+++ controller/userController.js
@@ -10,7 +10,33 @@ const loadRegister = async (req, res) => {
     }
 }
 
+const insertUser = async (req, res) => {
+    try {
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            mobile: req.body.mobile,
+            image: req.file.filename,
+            password: req.body.password,
+            is_admin: 0,
+        })
+
+        const userData = await user.save();
+
+        if (userData) {
+            res.render('registration',{message:'Registration Successful'})
+        }else{
+            res.render('registration',{message:'Registration Failed'})
+
+        }
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
 // Exports the function to allow it to be used by other parts of the application.
 module.exports = {
-    loadRegister
+    loadRegister,
+    insertUser
 }
