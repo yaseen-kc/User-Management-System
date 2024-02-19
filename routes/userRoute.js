9422c2a78f73c3b710389dd2652a9376ec1e8ac1
Muhammed Yaseen K C <ymkcmpd@gmail.com>
2024-02-19 19:01:59 +0530

    update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,43 +1,45 @@
-// Import necessary modules and packages
-const express = require('express'); // Import Express framework
-const user_route = express(); // Create a new Express router
+const express = require('express');
+const user_route = express();
+const session = require('express-session')
 
-// Set view engine and views directory for rendering EJS templates
+const config = require("../config/config")
+
+user_route.use(session({ secret: config.sessionSecret }))
+
 user_route.set('view engine', 'ejs');
 user_route.set('views', './views/users');
 
-const bodyParser = require('body-parser'); // Import body-parser for parsing request bodies
+const bodyParser = require('body-parser');
 user_route.use(bodyParser.json());
 user_route.use(bodyParser.urlencoded({ extended: true }));
 
-const multer = require('multer'); // Import multer for handling file uploads
+const multer = require('multer');
 const path = require('path');
 
-// Define storage settings for uploaded images
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../public/userImages')); // Set destination directory for uploaded images
+        cb(null, path.join(__dirname, '../public/userImages'));
     },
     filename: function (req, file, cb) {
-        const name = Date.now() + '-' + file.originalname; // Set filename with timestamp
-        cb(null, name); // Set filename for uploaded image
+        const name = Date.now() + '-' + file.originalname; stamp
+        cb(null, name);
     }
 });
 
-const upload = multer({ storage: storage }); // Create multer instance with defined storage settings
+const upload = multer({ storage: storage });
 
-const userController = require('../controller/userController'); // Import userController for handling user-related logic
-
-// Define routes for user registration
-user_route.get('/register', userController.loadRegister); // GET route to render registration form
-user_route.post('/register', upload.single('image'), userController.insertUser); // POST route to handle user registration
-
-user_route.get('/',userController.loginLoad);
-user_route.get('/login',userController.loginLoad);
-
-user_route.post('/login',userController.verifyLogin);
-
-user_route.get('/home',userController.loadHome)
+const userController = require('../controller/userController');
 
 
-module.exports = user_route; // Export user_route for use in main application
+user_route.get('/register', userController.loadRegister);
+user_route.post('/register', upload.single('image'), userController.insertUser);
+
+user_route.get('/', userController.loginLoad);
+user_route.get('/login', userController.loginLoad);
+user_route.post('/login', userController.verifyLogin);
+
+user_route.get('/home', userController.loadHome)
+
+
+module.exports = user_route; 
