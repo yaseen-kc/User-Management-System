cd1c19cb0f0f56048f18a7d751e202089589a230
Muhammed Yaseen K C <ymkcmpd@gmail.com>
2024-02-20 10:43:35 +0530

    Login Bug Fixed

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const user_route = express();
+
 const session = require('express-session')
 
 const config = require("../config/config")
+const auth = require("../middleware/auth")
 
-user_route.use(session({ secret: config.sessionSecret }))
+
+user_route.use(session({
+    secret: config.sessionSecret,
+    resave: false,
+    saveUninitialized: true
+}))
+
 
 user_route.set('view engine', 'ejs');
 user_route.set('views', './views/users');
+
+const userController = require("../controller/userController");
 
 const bodyParser = require('body-parser');
 user_route.use(bodyParser.json());
@@ -29,17 +39,17 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-const userController = require('../controller/userController');
 
 
-user_route.get('/register', userController.loadRegister);
+user_route.get('/register', auth.isLogout, userController.loadRegister);
 user_route.post('/register', upload.single('image'), userController.insertUser);
 
-user_route.get('/', userController.loginLoad);
-user_route.get('/login', userController.loginLoad);
+user_route.get('/', auth.isLogout, userController.loginLoad);
+
+user_route.get('/login', auth.isLogout, userController.loginLoad);
 user_route.post('/login', userController.verifyLogin);
 
-user_route.get('/home', userController.loadHome)
+user_route.get('/home', auth.isLogin, userController.loadHome)
 
 
 module.exports = user_route; 
