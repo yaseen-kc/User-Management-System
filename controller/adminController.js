b01bdc3dbb11577f4875297e218efd377cf04d29
Muhammed Yaseen K C <ymkcmpd@gmail.com>
2024-02-21 15:55:16 +0530

    update adminController.js and adminRoute.js

--- controller/adminController.js
+++ controller/adminController.js
@@ -61,10 +61,35 @@ const logout = async (req, res) => {
     }
 }
 
+const adminDashboard = async (req, res) => {
+    try {
+
+        var search = '';
+        if (req.query.search) {
+
+            search = req.query.search
+
+        }
+
+        const usersData = await User.find({
+            is_admin: 0,
+            $or: [
+                { name: { $regex: '.*' + search + '.*', $options: 'i' } },
+                { email: { $regex: '.*' + search + '.*', $options: 'i' } },
+                { mobile: { $regex: '.*' + search + '.*', $options: 'i' } }
+            ]
+        });
+        res.render('dashboard', { users: usersData })
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
 
 module.exports = {
     loadLogin,
     verifyLogin,
     loadDashboard,
-    logout
+    logout,
+    adminDashboard
 }
